5370b0b0a72128a4bf37c5ca0109656d613199f5
EricHdezITGuy <ehernandezm1@ucenfotec.ac.cr>
2023-02-23 18:46:17 -0600

    Agregamos un ciclo if para el tipo de cliente, adicional quisimos poner la diferencia si hay descuento

--- src/CalculadoraPrestamos.java
+++ src/CalculadoraPrestamos.java
@@ -5,8 +5,25 @@ import java.text.DecimalFormat;
 public class CalculadoraPrestamos {
     public static void main(String[] args) {
         // Crea un objeto Scanner para leer las entradas del usuario
-                Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         try {
+            // Leer el tipo de cliente
+            System.out.println("Bienvenido(a) a la calculadora de préstamos, por favor leer las siguientes instrucciones.\n");
+            System.out.println("Tipos de clientes:\n");
+            System.out.println("Tipo A1, A2: Crédito otorgado con un descuento del 2%.");
+            System.out.println("Tipo B: Crédito ordinario basado en cuotas establecidas.");
+            System.out.println("Tipo C: Crédito denegado.\n");
+            System.out.print("Ingrese el tipo de cliente (A1, A2, B, C): ");
+            String tipoCliente = input.next().toUpperCase();
+
+            double descuento = 0;
+            if (tipoCliente.equals("A1") || tipoCliente.equals("A2")) {
+                descuento = 0.02;
+            } else if (tipoCliente.equals("C")) {
+                System.out.println("Crédito denegado.");
+                return;
+            }
+
             // Leer el monto del préstamo
             System.out.print("Ingrese el monto del préstamo: ");
             double MontoPrestamo = input.nextDouble();
@@ -21,13 +38,16 @@ public class CalculadoraPrestamos {
 
             // Calcular el pago mensual
             // La fórmula es la siguiente:
-            // PagoMensual = MontoPrestamo * TasaInteres / (1 - 1 / Math.pow(1 + TasaInteres, CantidadAnios * 12));
-            double PagoMensual = MontoPrestamo * TasaInteres / (1 - 1 / Math.pow(1 + TasaInteres, CantidadAnios * 12));
+            // PagoMensual = (MontoPrestamo - MontoPrestamo * descuento) * TasaInteres / (1 - 1 / Math.pow(1 + TasaInteres, CantidadAnios * 12));
+            double PagoMensual = (MontoPrestamo - MontoPrestamo * descuento) * TasaInteres / (1 - 1 / Math.pow(1 + TasaInteres, CantidadAnios * 12));
 
             // Calcular el pago total
             // La fórmula es la siguiente:
             // PagoTotal = PagoMensual * CantidadAnios * 12;
             double PagoTotal = PagoMensual * CantidadAnios * 12;
+
+            // Calcular el monto del descuento
+            double MontoDescuento = MontoPrestamo * descuento;
 
             // Crear un objeto DecimalFormat
             DecimalFormat decimalFormat = new DecimalFormat("###,###.##");
@@ -37,7 +57,13 @@ public class CalculadoraPrestamos {
 
             // Salida del pago total
             System.out.println("Pago total: ₡" + decimalFormat.format(PagoTotal));
-        } catch (InputMismatchException e) {
+
+            // Salida del monto del descuento si corresponde
+            if (descuento > 0) {
+                System.out.println("Monto del descuento: ₡" + decimalFormat.format(MontoDescuento));
+            }
+
+         } catch (InputMismatchException e) {
             // Manejar la excepción InputMismatchException
             // Si el usuario ingresa una entrada que no es un número, se lanzará esta excepción
             System.out.println("Entrada inválida. Por favor, solamente inserte números.");
@@ -45,7 +71,7 @@ public class CalculadoraPrestamos {
             // Manejar cualquier otra excepción que pueda generarse
             System.out.println("Se ha producido un error. Inténtalo de nuevo.");
         } finally {
-            // Cierre el objeto Scanner para evitar fugas de recursos
+            // Cerramos el objeto Scanner para evitar fugas de recursos
             input.close();
         }
     }
